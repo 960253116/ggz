fed7290d49bb1a94781b24a56fba6879f299c971
ggz <960253116@qq.com>
2019-06-05 10:11:55 +0800

    0605

--- demo1/booktest/models.py
+++ demo1/booktest/models.py
@@ -4,10 +4,13 @@ from django.db import models
 class BookInfo(models.Model):
     title = models.CharField(max_length=20)
     pub_date=models.DateTimeField()
-
+    def __str__(self):
+        return self.title
 class HeroInfo(models.Model):
     name=models.CharField(max_length=20)
     gender=models.BooleanField(default=True)
     content=models.CharField(max_length=100)
     #book作为关键关联到bookinfo表
     book= models.ForeignKey(BookInfo,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
